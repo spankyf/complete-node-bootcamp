97eadc13ef0128da372b7ea4c9c09f487374edd6
spankyf <deanflanagan@gmail.com>
2020-05-07 15:10:22 -0400

    update

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,8 +4,21 @@ const url = require('url');
 
 // Building a simple server
 const server = http.createServer((req, res) =>{
-    console.log(req.url);
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    }   else if (pathName === '/product') {
+        res.end('This is the product');
+    }   else {
+        res.writeHead(404,{
+            'Content-type' : 'text/html',
+            'my-own-header':'hello-world'
+        });
+        res.end('<h1>This page could not be found</h1>');
+    }
+
+    // res.end('Hello from the server!');
 });
 
 server.listen(8000,'127.0.0.1',() => {
