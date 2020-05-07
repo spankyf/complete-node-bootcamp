72b1818dd79f5e40b62d70fac20ebc7c398680e1
spankyf <deanflanagan@gmail.com>
2020-05-07 13:50:13 -0400

    Progressing through course

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-// const process require('process');
-// console.log(process.cwd())
 
-// read a file
-const textIn = fs.readFileSync(path.join('starter','txt','input.txt'), 'utf-8');
-// console.log(textIn);
+// // Blockinng,synchronous way
+// const textIn = fs.readFileSync(path.join('starter','txt','input.txt'), 'utf-8');
+// // console.log(textIn);
 
-// write to file
-const textOut = `This is what we know about the avocado: ${textIn}. \nCreaated on ${Date.now()}`;
-fs.writeFileSync(path.join('starter','txt','output.txt'), textOut);
-console.log('File has been written');
+// // write to file
+// const textOut = `This is what we know about the avocado: ${textIn}. \nCreaated on ${Date.now()}`;
+// fs.writeFileSync(path.join('starter','txt','output.txt'), textOut);
+// console.log('File has been written');
+
+// read a file in async way
+fs.readFile(path.join('starter','txt','start.txt'), 'utf-8',(err, data1) => {
+    fs.readFile(path.join('starter','txt', `${data1}`+'.txt'), 'utf-8',(err, data2) => {
+        console.log('Should be here');
+        console.log(path.join('starter','txt','start.txt'));
+        console.log(data2)
+
+    });
+});
+
+console.log('Process underway...');
