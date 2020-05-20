b09b7bce1a7a6d83348b8910f6f17e04eeec2c1a
spankyf <deanflanagan@gmail.com>
2020-05-20 15:41:09 -0400

    Lecture 56 done

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -36,8 +36,17 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
 
+  // makes sure type is number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
   res.status(200).json({
-    status: 'success'
+    status: 'success',
+    data: { tour }
   });
 });
 
@@ -59,6 +68,30 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Update tour here'
+    }
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  // status 204 is send a null as its deleted
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
 
 app.listen(port, () => {
