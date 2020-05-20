55ad347815191f6dcbdc33d6759aea686dedc2c2
spankyf <deanflanagan@gmail.com>
2020-05-20 08:31:19 -0400

    added stuff

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -2,16 +2,34 @@ const superagent = require("superagent");
 const fs = require("fs");
 console.log("hi dean");
 
+// fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+//       console.log(res.body.message);
+
+//       fs.writeFile("dog-image.txt", res.body.message, (err) => {
+//         console.log("Random dog image saved to file");
+//       });
+//     });
+// });
+
+
 fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
   console.log(`Breed: ${data}`);
+  // the get method automatically returns a promise. In the beginnnig
+  // its a pending promise 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
+    .then(res => {
+      if(err) return console.log(err.message)
+      console.log(res.body.message)
 
       fs.writeFile("dog-image.txt", res.body.message, (err) => {
+        if (err) return console.log(err.message);
         console.log("Random dog image saved to file");
       });
-    });
+    })
 });
