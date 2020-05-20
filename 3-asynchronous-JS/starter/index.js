a76c60ea0abbe4e922df6e85d1f54a4cfef0f613
spankyf <deanflanagan@gmail.com>
2020-05-20 08:38:42 -0400

    installed extensions

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,6 +1,6 @@
 const superagent = require("superagent");
 const fs = require("fs");
-console.log("hi dean");
+
 
 // fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
 //   console.log(`Breed: ${data}`);
@@ -24,12 +24,13 @@ fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then(res => {
-      if(err) return console.log(err.message)
       console.log(res.body.message)
 
       fs.writeFile("dog-image.txt", res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log("Random dog image saved to file");
-      });
+      }); 
+    }).catch(err => {
+      console.log(err.message)
     })
 });
