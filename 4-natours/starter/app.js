cc5ce27e5dd51e6f2f5d80810776285b6b2bedb8
spankyf <deanflanagan@gmail.com>
2020-05-20 14:17:49 -0400

    Lecture 54

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
+// the line belwo is for middleware which is between request and response
+// data from request added to response
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -17,7 +21,46 @@ const tours = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'))
 );
 
+console.log(tours.length);
+
 const port = 3000;
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours: tours }
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: 'success'
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  // Frist figure out what the id will be. Lets work it out.
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+    JSON.stringify(tours),
+    (err) => {
+      // status 201 is created new resourse
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour }
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
