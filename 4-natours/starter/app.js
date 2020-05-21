7ac13ff1cfa8e97b9d5604de5243a342f923ced3
spankyf <deanflanagan@gmail.com>
2020-05-21 09:34:06 -0400

    Added mogran

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 app.use(express.json());
