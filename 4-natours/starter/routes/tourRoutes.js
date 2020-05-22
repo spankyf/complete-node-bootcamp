fb147960991a3b7792c3cdf36f7bd86aedf4c18d
spankyf <deanflanagan@gmail.com>
2020-05-22 15:35:48 -0400

    Lecture 66 finished

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
+// router.param('name', tourController.checkBody);
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(
+    tourController.checkBody,
+    tourController.createTour
+  );
 
 router
   .route('/:id')
