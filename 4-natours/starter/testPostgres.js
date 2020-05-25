c9ef42d387d8a469c2b6802f4e2721aa7041a8f3
spankyf <deanflanagan@gmail.com>
2020-05-25 10:10:01 -0400

    Set up pg db

--- 4-natours/starter/testPostgres.js
+++ 4-natours/starter/testPostgres.js
@@ -1,6 +1,12 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL
+  connectionString:
+    'postgres://ovkqqiatxiqyza:99d58e78af22b2d82786bb0d713c48a753e4c625ef19257d1beb3fd0c2e692b7@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/d938vm8og3qm59',
+  ssl: { rejectUnauthorized: false }
 });
 client.connect();
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  client.end();
+});
