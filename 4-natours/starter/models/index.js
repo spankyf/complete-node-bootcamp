563f6cc052915fb731143a2541ba037892236928
spankyf <deanflanagan@gmail.com>
2020-05-27 22:44:46 -0400

    Got sequelize going

---
+++ 4-natours/starter/models/index.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+const Sequelize = require('sequelize');
+
+dotenv.config({ path: './starter/config.env' });
+
+const URI = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+const sequelize = new Sequelize(URI, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// db.tutorials = require('./tutorial.model.js')(
+//   sequelize,
+//   Sequelize
+// );
+
+module.exports = db;
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,17 @@
 const dotenv = require('dotenv');
-const { Client } = require('pg');
+const bodyParser = require('body-parser');
+const app = require('./app');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require('./models');
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and re-sync db.');
+});
 
 dotenv.config({ path: './starter/config.env' });
-const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
