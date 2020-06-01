684842950fbbe331c2cd978d5e40be6d2d6fa611
spankyf <deanflanagan@gmail.com>
2020-06-01 09:13:25 -0400

    Last attempt laptop

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -1,25 +1,73 @@
+const dotenv = require('dotenv');
 const fs = require('fs');
-const db = require('../../models');
+const Sequelize = require('sequelize');
+const sequelizeTransforms = require('sequelize-transforms');
 
-const Tour = db.tours;
-
-const tours = JSON.parse(
-  fs.readFileSync('tours-simple.json')
+dotenv.config({ path: './config.env' });
+// sconsole.log(process.env);
+const URI = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
-const createTour = async (req, res) => {
-  // Validate request
-  try {
-    const newTour = await Tour.create(req.body);
+const sequelize = new Sequelize(URI, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
-    res.status(201).json({
-      status: 'success',
-      data: { tour: newTour }
-    });
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+sequelizeTransforms(sequelize);
+db.sequelize.sync().then(() => {
+  console.log('Drop and re-sync db.');
+});
+// db.sequelize.sync().then(() => {
+//   console.log('Drop and re-sync db.');
+// });
+const Tour = db.tours;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`)
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+
+    console.log('Date inputted successfully');
   } catch (err) {
-    res.status(400).json({
-      status: 'fail',
-      message: err
-    });
+    console.log(err);
   }
 };
+
+const deleteData = async () => {
+  try {
+    await Tour.destroy({
+      where: {},
+      truncate: true
+    });
+
+    console.log('Data delete successfully');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === 'import') {
+  importData();
+} else if (process.argv[2] === '---delete') {
+  deleteData();
+}
+
+console.log(process.argv);
