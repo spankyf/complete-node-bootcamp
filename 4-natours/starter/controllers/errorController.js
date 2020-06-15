94cbcdb87653ffd32002e017c2d854db9a0e7b8b
spankyf <deanflanagan@gmail.com>
2020-06-15 13:25:48 -0400

    Lecture 119 done

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,3 +1,19 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  //console.log(err);
+  const message = `Invalid ${err.original} : ${err.name}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errors[0];
+  //console.log(err.errors[0].message);
+  //console.log(value.value);
+  const message = `Duplicate fields for ${value.value} . Use another value`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({ status: err.status, error: err, message: err.message, stack: err.stack });
 };
@@ -20,6 +36,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'SequelizeDatabaseError') error = handleCastErrorDB(error);
+    if (error.name === 'SequelizeValidationError') error = handleDuplicateFieldsDB(error);
+    //if (error.errors[0].message.name === 'Invalid validator function: 1') error = handleDuplicateFieldsDB(error);
+    sendErrorProd(error, res);
   }
 };
